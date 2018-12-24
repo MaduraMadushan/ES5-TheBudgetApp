396953bd4c1ab48ba02cb359098a183aab05fa01
MaduraMadushan <39506813+MaduraMadushan@users.noreply.github.com>
2018-12-24 10:17:08 +0530

    Finishing Touches_ Improving the UX

--- app.js
+++ app.js
@@ -136,6 +136,12 @@ var UIController = (function(){
         dateLabel: '.budget__title--month'
     };
 
+    var nodeListForEach = function(list, callback){
+        for (var i = 0; i < list.length; i++){
+            callback(list[i], i);
+        }
+    };
+
     var formatNumber = function(num, type){
         var numSplit, int, dec, type;
         num = Math.abs(num);
@@ -209,11 +215,7 @@ var UIController = (function(){
         displayPercentages: function(percentages){
             var fields = document.querySelectorAll(DOMstrings.expensesPercentageLabel);
 
-            var nodeListForEach = function(list, callback){
-                for (var i = 0; i < list.length; i++){
-                    callback(list[i], i);
-                }
-            };
+           
 
             nodeListForEach(fields, function(current, index){
                 if(percentages[index] > 0){
@@ -233,6 +235,19 @@ var UIController = (function(){
             year = now.getFullYear();
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
 
+        },
+        changedType: function(){
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue
+            );
+
+            nodeListForEach(fields, function(cur){
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputButton).classList.toggle('red');
         },
         getDOMstrings: function(){
             return DOMstrings;
@@ -256,7 +271,8 @@ var controller = (function(budgetCtrl, UICtrl){
             }
         });
 
-        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem)
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
 
     var updateBudget = function(){
