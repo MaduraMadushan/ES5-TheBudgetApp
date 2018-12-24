2d63c0ab2bf4263b78d57d7360f62477b2f43044
MaduraMadushan <39506813+MaduraMadushan@users.noreply.github.com>
2018-12-24 08:58:19 +0530

    Formatting Numbers

--- app.js
+++ app.js
@@ -135,6 +135,21 @@ var UIController = (function(){
         expensesPercentageLabel: '.item__percentage'
     };
 
+    var formatNumber = function(num, type){
+        var numSplit, int, dec, type;
+        num = Math.abs(num);
+        num = num.toFixed(2);
+
+        numSplit = num.split('.');
+        int = numSplit[0];
+        if(int.length > 3){
+           int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);
+        }
+        dec = numSplit[1];
+
+        return (type === 'exp' ? '-' : '+' ) + ' ' + int + '.' + dec;
+    };
+
     return {
         getinput: function(){
             return{
@@ -158,7 +173,7 @@ var UIController = (function(){
 
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%', obj.description);
-            newHtml = newHtml.replace('%value%', obj.value);
+            newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
 
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
